be4c1c4d40a2c51be5029510bd2115b6d93c88c4
KhoiDaDen1906 <tin393922@gmail.com>
2026-08-19 08:34:46 +0700

    lkshdfkjsdhf

--- Exercise1/obj/Debug/net10.0/Test3.AssemblyInfo.cs
+++ Exercise1/obj/Debug/net10.0/Test3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+165dbab7dbece191ddf42f6e1032fb3d79980584")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
